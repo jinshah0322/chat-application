517e700466f2365d28ec2cef49bee698f997bdb1
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2023-12-23 14:31:05 +0530

    created user and chat model

---
+++ models/chatModel.js
@@ -0,0 +1,17 @@
+const { Timestamp } = require('mongodb');
+const mongoose = require('mongoose'); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var chatSchema = new mongoose.Schema({
+    sender_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    receiver_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true});
+
+//Export the model
+module.exports = mongoose.model('Chat', chatSchema);
