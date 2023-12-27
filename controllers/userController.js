41cb8d3e61ba74cc564a5c2ab8e3680d05ac1b03
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2023-12-27 14:23:07 +0530

    Update register.ejs and userRoute.js for file upload and editprofile functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const bcryptjs = require("bcryptjs")
 const sendEmail = require("../helper/sendEmail")
 const mongoose = require("mongoose")
 const Chat = require("../models/chatModel")
+
 const registerLoad = async (req,res)=>{
     try{
         res.render("register")
@@ -288,7 +289,8 @@ const changePassword = async(req,res)=>{
 
 const loadEditProfile = async(req,res)=>{
     try{
-        const user = req.session.user
+        const userId = req.session.user._id
+        const user = await User.findOne({_id:userId})
         res.render("editprofile",{user:user})
     }catch(error){
         console.log(error.message);
@@ -300,39 +302,63 @@ const editProfile = async(req,res)=>{
         const {username,email,mobile} = req.body
         const userId = req.session.user._id
         const user = await User.findOne({_id:userId})
-        const existingUserEmail = await User.findOne({ email: email });
-        const existingUserNumber = await User.findOne({ mobile: mobile })
-        if(!existingUserEmail){
-            if(!existingUserNumber){
-                if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)){
-                    if(/^(\+\d{1,3}[- ]?)?\d{10}$/.test(mobile)){
-                        const oldEmail = user.email
-                        const updatedUser = await User.updateOne({email:oldEmail},{username,email,mobile})
-                        const html = `
-                                <h2>Your Profile has been Updatedn </h2>
-                                <p>Dear ${username},</p>
-                                <p>We want to inform you that your profile on our application has been successfully updated. Your changes have been saved, and your profile now reflects the updated information.If you have any further updates or need assistance, feel free to log in to your account and make the necessary changes. If you have any questions, please contact our support team at <a href="mailto:jinshah0322@gmail.com">jinshah0322@gmail.com</a></p>
-                                <p>Best regards,<br>[Jinay Shah]</p>
-                            `
-                            var data = {
-                                to: email,
-                                text: `Hey ${username}`,
-                                subject: "Your Profile has been Updated",
-                                html: html
-                            }
-                            sendEmail(data)
-                            data = {
-                                to: oldEmail,
-                                text: `Hey ${username}`,
-                                subject: "Your Profile has been Updated",
-                                html: html
-                            }
-                            sendEmail()
-                    }else{res.render('register',{message:'Enter Valid moile number'})}
-                } else{res.render('register',{message:'Enter Valid email address'})}
-            } else{res.render('editprofile',{message:'User with same Mobile Number already exist'})}
-        } else{res.render('editprofile',{message:'User with same email address already exist'})}
-        res.redirect("/profile")
+        if(username){
+            var usernameupdate = User.updateOne({_id:userId},{username:username})
+        }
+        if (req.file) {
+            var userimage = User.updateOne({_id:userId},{image:"images/"+req.file.filename})
+        }
+        if(mobile){
+            if(/^(\+\d{1,3}[- ]?)?\d{10}$/.test(mobile)){
+                const allNumber = await User.find({mobile:mobile})
+                if(allNumber.length == 0){
+                    var numberupdate = User.updateOne({_id:userId},{mobile:mobile})
+                } else{
+                    res.josn({redirectUrl: "/editprofile",message:'User with same Mobile Number already exist'})
+                }
+            } else{
+                res.josn({redirectUrl: "/editprofile",message:'Enter Valid moile number'})
+            }
+        }
+        if(email){
+            if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)){
+                const allEmail = await User.find({email:email})
+                if(allEmail.length == 0){
+                    var emailupdate = User.updateOne({_id:userId},{email:email})
+                } else{
+                    res.josn({redirectUrl: "/editprofile",message:'User with same email address already exist'})
+                }   
+            } else{
+                res.josn({redirectUrl: "/editprofile",message:'Enter Valid email address'})
+            }
+        }
+        await userimage
+        await usernameupdate
+        await numberupdate
+        await emailupdate
+        const oldEmail = user.email
+        const updatedUser = await User.updateOne({email:oldEmail},{username,email,mobile})
+        const html = `
+                <h2>Your Profile has been Updatedn </h2>
+                <p>Dear ${username},</p>
+                <p>We want to inform you that your profile on our application has been successfully updated. Your changes have been saved, and your profile now reflects the updated information.If you have any further updates or need assistance, feel free to log in to your account and make the necessary changes. If you have any questions, please contact our support team at <a href="mailto:jinshah0322@gmail.com">jinshah0322@gmail.com</a></p>
+                <p>Best regards,<br>[Jinay Shah]</p>
+            `
+        var data = {
+            to: email,
+            text: `Hey ${username}`,
+            subject: "Your Profile has been Updated",
+            html: html
+        }
+        sendEmail(data)
+        data = {
+            to: oldEmail,
+            text: `Hey ${username}`,
+            subject: "Your Profile has been Updated",
+            html: html
+        }
+        sendEmail()
+        res.json({ redirectUrl: "/profile" });
     }catch(error){
         console.log(error);
     }
